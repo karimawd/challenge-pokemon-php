76436224f9861026b5a1ba72223b7236d931689d
karimawd <klaamiri2018@gmail.com>
2020-07-14 15:29:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Ajax Pokédex">
+    <meta name="keywords" content="Pokémon game">
+    <meta name="author" content="Yuri & Said">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Pokémon Game</title>
+</head>
+<body>
+
+<div class="container">
+    <!-- Header -->
+    <header>
+        <span><img src="img/pokeball.png"></span><img src="img/pokemon.png" id="pokemon"><span><img src="img/pokeball.png"></span></h1>
+    </header>
+    <!-- Main -->
+    <main>
+        <section class="input-field">
+            <input id="input" type="text" autocomplete="off" name="poke" placeholder="ID or Name Pokemon">
+            <button id="button" aria-label="startSearch" type="button" class="btn btn-danger">I choose you</button>
+        </section>
+        <section id="background">
+            <div class="img-text" id="img-photo">
+                <div id="pokeId" class="text-center"></div>
+                <div id="name" class="text-center"></div>
+                <div id="sprite"></div>
+                <div id="moves" class="text-center"></div>
+                <button id="prev" aria-label="previousSearch" type="button" class="btn btn-primary"></button>
+                <button id="next" aria-label="nextSearch" type="button" class="btn btn-danger"></button>
+            </div>
+        </section>
+    </main>
+</div>
+
+</body>
+</html>
